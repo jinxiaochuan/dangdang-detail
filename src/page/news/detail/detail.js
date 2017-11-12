fa88712603be61945472545a66c4a2d230e06e19
DaiZhiTao <m13007230048@163.com>
2017-11-12 18:44:37 +0800

    uncomment

--- src/page/news/detail/detail.js
+++ src/page/news/detail/detail.js
@@ -31,8 +31,8 @@ var News = jsmod.util.klass({
     getAjax: function(){
         var self = this;
 
-        HREF_ORIGIN = 'http://dev.im-dangdang.com/news/v1/news/detail?userId=200119&newsId=1';
-        URL_NEWS = 'http://dev.im-dangdang.com/ddweb/v1/news/detail';
+        // HREF_ORIGIN = 'http://dev.im-dangdang.com/news/v1/news/detail?userId=200119&newsId=1';
+        // URL_NEWS = 'http://dev.im-dangdang.com/ddweb/v1/news/detail';
 
         var data = {};
 
