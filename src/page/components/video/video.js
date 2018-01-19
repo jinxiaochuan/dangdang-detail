b2a44bb1e825330fda3c430ccd016f9e04a48c5c
靳小川 <18251859801@163.com>
2018-01-19 18:32:43 +0800

    video

--- src/page/components/video/video.js
+++ src/page/components/video/video.js
@@ -4,6 +4,13 @@ require('./video.less');
 
 var Vue = require('vue');
 
+Vue.filter('formatTime', function (value) {
+  if (!value) return '00:00'
+  var min = parseInt(value/60) > 9 ? parseInt(value/60) : '0' + parseInt(value/60);
+  var sec = parseInt(value%60) > 9 ? parseInt(value%60) : '0' + parseInt(value%60);
+  return min + ':' + sec
+})
+
 module.exports = Vue.extend({
     name: 'videoComponent',
 
@@ -11,17 +18,129 @@ module.exports = Vue.extend({
 
     data: function () {
         return {
-
+            canplay: false, // 是否可以播放
+            loading: true, // 是否处于加载中
+            playing: false, // 是否处于播放中
+            controlBar: false, // 是否展示控制面板
+            isPause: false, // 是否处于暂停状态
+            playError: false, // 是否播放时发生错误
+            duration: '', // 总时长
+            currentTime: '', // 当前时间
         }
     },
 
     methods: {
+        videoPlay () {
+            this.$refs.videoPlayer.play();
+        },
 
+        tapVideo () {
+            this.controlBar = !this.controlBar;
+        },
+
+        togglePlay () {
+            console.log('togglePlay');
+            if(this.$refs.videoPlayer.paused){
+                this.$refs.videoPlayer.play();
+            }else {
+                this.$refs.videoPlayer.pause();
+            }
+        },
+
+        retry () {
+
+        },
+
+        eventListener () {
+            var self = this;
+
+            // 客户端开始请求数据
+            this.$refs.videoPlayer.addEventListener('loadstart', function(){
+                console.log('客户端开始请求数据');
+            }, false)
+
+            // 客户端正在请求数据
+            this.$refs.videoPlayer.addEventListener('progress', function(){
+                console.log('客户端正在请求数据');
+            }, false)
+
+            // 延迟下载
+            this.$refs.videoPlayer.addEventListener('suspend', function(){
+                console.log('延迟下载');
+            }, false)
+
+            // 客户端主动终止下载（不是因为错误引起）
+            this.$refs.videoPlayer.addEventListener('abort', function(){
+                console.log('客户端主动终止下载（不是因为错误引起）');
+            }, false)
+
+            // 请求数据时遇到错误
+            this.$refs.videoPlayer.addEventListener('error', function(){
+                console.log('请求数据时遇到错误');
+                self.playError = true;
+                self.controlBar = false;
+            }, false)
+
+            // 网速失速
+            this.$refs.videoPlayer.addEventListener('stalled', function(){
+                console.log('网速失速');
+            }, false)
+
+            // play()和autoplay开始播放时触发
+            this.$refs.videoPlayer.addEventListener('play', function(){
+                console.log('play()和autoplay开始播放时触发');
+                self.isPause = false;
+            }, false)
+
+            // pause()触发
+            this.$refs.videoPlayer.addEventListener('pause', function(){
+                console.log('pause()触发');
+                self.isPause = true;
+            }, false)
+
+            // 成功获取资源长度
+            this.$refs.videoPlayer.addEventListener('loadedmetadata', function(){
+                console.log('成功获取资源长度');
+            }, false)
+
+            // 等待数据，并非错误
+            this.$refs.videoPlayer.addEventListener('waiting', function(){
+                console.log('等待数据，并非错误');
+            }, false)
+
+            // 播放中
+            this.$refs.videoPlayer.addEventListener('playing', function(){
+                console.log('播放中');
+            }, false)
+
+            // 可以播放，但中途可能因为加载而暂停
+            this.$refs.videoPlayer.addEventListener('canplay', function(){
+                console.log('可以播放，但中途可能因为加载而暂停');
+            }, false)
+
+            // 可以播放，歌曲全部加载完毕
+            this.$refs.videoPlayer.addEventListener('canplaythrough', function(e){
+                console.log('可以播放，歌曲全部加载完毕');
+                this.pause();
+                self.playError = false;
+                self.loading = false;
+                self.canplay = true;
+                self.duration = this.duration;
+            }, false)
+
+            // 播放时间改变
+            this.$refs.videoPlayer.addEventListener('timeupdate', function(e){
+                console.log('播放时间改变');
+                self.currentTime = this.currentTime;
+            }, false)
+
+
+        }
     },
 
     mounted: function(){
         this.$nextTick(() =>{
-            
+            this.eventListener()
         })
     }
 })
--- src/page/components/video/video.tpl
+++ src/page/components/video/video.tpl
@@ -1,35 +1,45 @@
 <div class="video-player-wrap">
     <div class="video-player">
-        <video webkit-playsinline="true" playsinline="true" preload="metadata" poster src="http://v4.music.126.net/20180120020412/39bf3b26fb32945d63101fb4807579c5/web/cloudmusic/ICBgYWA0IDYzMCEiIDAiMA==/mv/5780003/9cf8911807c98ba7474d8d6ef5f6245e.mp4" autoplay poster="posterimage.jpg">
+        <video ref="videoPlayer" v-show="canplay" webkit-playsinline="true" playsinline="true" preload="metadata" poster src="http://v4.music.126.net/20180120020412/39bf3b26fb32945d63101fb4807579c5/web/cloudmusic/ICBgYWA0IDYzMCEiIDAiMA==/mv/5780003/9cf8911807c98ba7474d8d6ef5f6245e.mp4" autoplay poster="posterimage.jpg">
 
         </video>
     </div>
-    <div class="video-title">
+    <div v-show="loading" class="video-title">
         等你下课 - 周杰伦
     </div>
-    <div class="video-poster">
+    <div v-show="loading" class="video-poster">
         <img src="http://p3.music.126.net/OFhwUU6ozSVveVsACccADw==/109951163111179433.webp?imageView&thumbnail=600x0&quality=75&tostatic=0&type=webp" alt="">
     </div>
-    <div class="video-button">
-        <div class="video-play-ico x-iconmode-new">
+    <div @click="tapVideo" class="video-button">
+        <div v-show="isPause" @click="videoPlay" class="video-play-ico x-iconmode-new">
 
         </div>
     </div>
     <div class="x-iconmode-new video-youkulogo">
 
     </div>
-    <div class="x-dashboard">
+    <div v-show="loading" class="x-video-loading">
+
+    </div>
+    <div v-show="playError" class="x-noticeshow">
+        <div class="x-errortip">
+            <div class="x-youkulogo x-iconmode"></div>
+            <span>视频播放失败，请重试</span>
+            <div @click="retry" class="x-error-retry x-iconmode"></div>
+        </div>
+    </div>
+    <div class="x-dashboard" :class="{'show': controlBar}">
         <div class="x-console-out">
             <div class="x-console">
-                <div class="x-iconmode-new i_pause">
+                <div @click="togglePlay" class="x-iconmode-new" :class="[{'i_play': isPause}, {'i_pause': !isPause}]">
 
                 </div>
                 <div class="x-progress">
-                    <span class="x-playtime">00:43</span>
+                    <span class="x-playtime">{{ currentTime|formatTime }}</span>
                     <div class="x-progress-track">
-                        <span class="x-progress-play" style="width:15px;"><i class="seekBtn"></i></span>
+                        <span class="x-progress-play" :style="{'width': currentTime/duration * 165 + 16 + 'px'}"><i class="seekBtn"></i></span>
                     </div>
-                    <span class="x-alltime">01:10</span>
+                    <span class="x-alltime">{{ duration|formatTime }}</span>
                 </div>
                 <div class="x-iconmode i_fscreen">
 
