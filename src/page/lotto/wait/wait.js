737f55dfba1db0f3a326794de59f4b398575c6d7
靳小川 <18251859801@163.com>
2018-01-22 16:11:47 +0800

    红包天天乐 - 空窗

--- src/page/lotto/wait/wait.js
+++ src/page/lotto/wait/wait.js
@@ -6,6 +6,18 @@ require('./wait.less');
 
 var TPL_WAIT = require('./tmpls/wait.tpl');
 
+var jsmod = require('lib/self/jsmod/jsmod_extend.js');
+
+var setupWebViewJavascriptBridge = require('lib/self/setupWebViewJavascriptBridge.js');
+
+var HREF_ORIGIN = window.location.href;
+
+var PATH_ORIGIN = window.location.origin;
+
+var PATH_NAME = '/ddweb/v1/ttl/hb/cardNum';
+
+var URL_LOTTO = PATH_ORIGIN + PATH_NAME;
+
 new Vue({
     el: '#lotto-wait',
 
@@ -13,14 +25,33 @@ new Vue({
 
     data: function () {
         return {
-
+            cardNum: 0
         }
     },
 
     methods: {
 
         init () {
+            var self = this;
 
+            // HREF_ORIGIN = 'http://dev.im-dangdang.com/ddweb/ttl/wait?userId=200119';
+            // URL_LOTTO = 'http://dev.im-dangdang.com/ddweb/v1/ttl/hb/cardNum';
+
+            var data = {};
+
+            data.userId = jsmod.util.url.getParam(HREF_ORIGIN,'userId');
+
+            $.ajax({
+                url: URL_LOTTO,
+                dataType: 'jsonp',
+                data: data,
+                jsonp: 'callback',
+                success: function(json){
+                    if(json.status == 1){
+                        self.cardNum = json.data.cardNum;
+                    }
+                }
+            })
         }
 
     },
