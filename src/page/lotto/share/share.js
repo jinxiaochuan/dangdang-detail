d067f22997da6adb205e7397e3fff375d8f20fc2
靳小川 <18251859801@163.com>
2018-02-12 14:36:27 +0800

    红包天天乐 - 分享

--- src/page/lotto/share/share.js
+++ src/page/lotto/share/share.js
@@ -6,9 +6,19 @@ require('./share.less');
 
 var TPL_SHARE = require('./tmpls/share.tpl');
 
+var jsmod = require('lib/self/jsmod/jsmod_extend.js');
+
 var share = require('lib/self/share.js');
 
 var setupWebViewJavascriptBridge = require('lib/self/setupWebViewJavascriptBridge.js');
+
+var HREF_ORIGIN = window.location.href;
+
+var PATH_ORIGIN = window.location.origin;
+
+var PATH_NAME = '/ddweb/v1/ttl/share/activity/detail';
+
+var URL_LOTTO = PATH_ORIGIN + PATH_NAME;
 
 new Vue({
     el: '#lotto-share',
@@ -17,12 +27,14 @@ new Vue({
 
     data: function () {
         return {
-            activityInfo: null,
-            pictureUrl: '',
-            cardNum: 0,
-            code: '',
-            hasCard: null,
-            baseInfo: null
+            userImage: '',
+            showName: '',
+            shareGreeting: '',
+            showNo: '',
+            shareCode: '',
+            shareStory: '',
+            shareTitle: '',
+            code: ''
         }
     },
 
@@ -30,7 +42,43 @@ new Vue({
         init () {
             var self = this;
 
+            HREF_ORIGIN = 'http://dev.im-dangdang.com/ddweb/ttl/share/activity/detail?userId=200119&shareHbId=1'
+            URL_LOTTO = 'http://dev.im-dangdang.com/ddweb/v1/ttl/share/activity/detail'
 
+            var data = {};
+
+            data.userId = jsmod.util.url.getParam(HREF_ORIGIN,'userId');
+            data.shareHbId = jsmod.util.url.getParam(HREF_ORIGIN,'shareHbId');
+
+            $.ajax({
+                url: URL_LOTTO,
+                dataType: 'jsonp',
+                data: data,
+                jsonp: 'callback',
+                success: function(json){
+                    if(json.status == 1){
+                        self.userImage = json.data.userInfo.userImage;
+                        self.showName = json.data.userInfo.showName;
+                        self.shareGreeting = json.data.shareInfo.shareGreeting;
+                        self.showNo = json.data.shareInfo.showNo;
+                        self.shareCode = json.data.shareInfo.shareCode;
+                        self.shareStory = json.data.shareInfo.shareStory;
+                        self.shareTitle = json.data.shareInfo.shareTitle;
+                        self.code = json.data.codeInfo.code;
+                        self.initTitle();
+                        self.initShare();
+                    }
+                }
+            })
+
+        },
+
+        initTitle () {
+            document.title = this.shareTitle;
+        },
+
+        initShare () {
+            share();
         }
     },
 
--- src/page/lotto/share/tmpls/share.tpl
+++ src/page/lotto/share/tmpls/share.tpl
@@ -4,23 +4,23 @@
     </div>
     <div class="share-detail">
         <div class="avatar">
-            <img src="http://s1.im-dangdang.com/test/20180206/ea24d3974a1f1dabc34d499a6fa8f07b.gif?x-oss-process=image/resize,m_fill,h_100,w_100,limit_0" alt="">
+            <img :src="userImage" alt="">
         </div>
-        <span class="name">唐彤彤</span>
-        <span class="bless">我给在铛铛领取了一个红包，分享给你。你也快来抢吧我给在铛铛领取了一个红包，分享给你。你也快来抢吧我给在铛铛领取了一个红包，分享给你。你也快来抢吧我给在铛铛领取了一个红包，分享给你。</span>
+        <span class="name">{{ showName }}</span>
+        <span class="bless">{{ shareGreeting }}</span>
         <div class="share-envelope">
-            <span class="num">88</span>
+            <span class="num">{{ showNo }}</span>
             <span class="share-receive">
-                <span class="code">WNH9021</span>
+                <span class="code">{{ shareCode }}</span>
                 <span class="word">红包领取码</span>
             </span>
         </div>
         <span class="guide">【发现】—【红包天天乐】—【…】—【领取红包】</span>
-        <span class="story">故事，这个故事是个很长的故事，从前的从前有那么一个人，他为了爱情…故事，这个故事是个很长的故事，从前的从前有那么一个人，他为了爱情…故事，这个故事是个很长的故事，从前的从前有那么一个人，他为了爱情…故事，这个故事是个很长的故事，从前的从前有那么一个人，他为了爱情…</span>
+        <span class="story">{{ shareStory }}</span>
         <div class="share-invite-wrap">
             <span class="code-word">使用我的邀请码，均可获得一张红包卡</span>
             <div class="share-invite">
-                <span class="invite-code">SHN889</span>
+                <span class="invite-code">{{ code }}</span>
             </div>
         </div>
     </div>
