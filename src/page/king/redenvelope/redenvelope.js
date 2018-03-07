d5bd32d7959c2266f1aada20b28def5aac43c5f5
靳小川 <18251859801@163.com>
2018-03-07 16:00:32 +0800

    红包王

---
+++ src/page/king/redenvelope/redenvelope.js
@@ -0,0 +1,97 @@
+var Vue = require('vue');
+
+require('page/common/common.js');
+
+require('./redenvelope.less');
+
+var TPL_REDENVELOPE = require('./tmpls/redenvelope.tpl');
+
+var jsmod = require('lib/self/jsmod/jsmod_extend.js');
+
+var share = require('lib/self/share.js');
+
+var setupWebViewJavascriptBridge = require('lib/self/setupWebViewJavascriptBridge.js');
+
+var HREF_ORIGIN = window.location.href;
+
+var PATH_ORIGIN = window.location.origin;
+
+var PATH_NAME = '/ddweb/v1/ttl/hb/detail';
+
+var URL_KING = PATH_ORIGIN + PATH_NAME;
+
+Vue.filter('amount_convert',function(val){
+    return (val/100).toFixed(2)
+})
+
+new Vue({
+    el: '#king-red-envelope',
+
+    template: TPL_REDENVELOPE,
+
+    data: function () {
+        return {
+            record: null,
+            recordBg: '',
+            bridge: ''
+        }
+    },
+
+    methods: {
+        init () {
+            var self = this;
+
+            HREF_ORIGIN = 'http://app.im-dangdang.com/ddweb/ttl/hb/detail?winId=10274&userId=1000034&shareType=12&shareId=10274&shareUserId=1000034&source=1'
+            URL_KING = 'http://app.im-dangdang.com/ddweb/v1/ttl/hb/detail'
+
+            var data = {}, shareUserId;
+
+            data.userId = jsmod.util.url.getParam(HREF_ORIGIN,'userId');
+            data.activityId = jsmod.util.url.getParam(HREF_ORIGIN,'activityId');
+            data.winId = jsmod.util.url.getParam(HREF_ORIGIN,'winId');
+            shareUserId = jsmod.util.url.getParam(HREF_ORIGIN,'shareUserId');
+            shareUserId && (data.shareUserId = shareUserId)
+
+            $.ajax({
+                url: URL_KING,
+                dataType: 'jsonp',
+                data: data,
+                jsonp: 'callback',
+                success: function(json){
+                    if(json.status == 1){
+                        self.record = json.data.record;
+                        self.recordBg = json.data.record.hbBackgroundImage && JSON.parse(json.data.record.hbBackgroundImage).picture || 'http://s1.im-dangdang.com/online/20180227/bg_share_star_dream.png'
+                        self.initShare();
+                        self.initBridge();
+                    }
+                }
+            })
+
+        },
+
+        initShare () {
+            share();
+        },
+
+        initBridge () {
+            var self = this;
+            setupWebViewJavascriptBridge(function(bridge){
+                self.bridge = bridge;
+                self.bridge.callHandler('baseInfo', self.record, function(){})
+            })
+        },
+
+        tapUser () {
+            if(!this.bridge) return
+            this.bridge.callHandler('tapUser', this.record, function(){})
+        }
+
+    },
+
+    mounted: function () {
+        this.$nextTick(() => {
+            this.init()
+        })
+    }
+
+})
---
+++ src/page/king/redenvelope/tmpls/redenvelope.tpl
@@ -0,0 +1,20 @@
+<div class="king-red-envelope-wrap">
+    <div class="envelope-top">
+
+    </div>
+    <div v-if="record" class="envelope-detail">
+        <div @click="tapUser" class="avatar">
+            <img :src="record.userImage" alt="">
+        </div>
+        <span class="name">{{ record.userName }}</span>
+        <span class="cash-wrap">
+            <span class="cash-amount"><span class="amount-num">{{ record.amount|amount_convert }}</span>元</span>
+        </span>
+    </div>
+    <div v-if="record" class="envelope-star" v-bind:style="{backgroundImage: 'url('+ recordBg +')'}">
+        <div class="envelope-code">
+            <span class="word">使用我的红包码，均可获得一张红包卡</span>
+            <span class="code">{{ record && record.code }}</span>
+        </div>
+    </div>
+</div>
